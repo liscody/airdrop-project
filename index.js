a8b5a3c7976579d878d018ed7ca3911b250a1797
Igor A <iantsupov@gmail.com>
2022-06-29 08:28:34 +0300

    init

---
+++ index.js
@@ -0,0 +1,52 @@
+const {
+    Connection,
+    PublicKey,
+    clusterApiUrl,
+    Keypair,
+    LAMPORTS_PER_SOL
+} = require("@solana/web3.js")
+
+const wallet = new Keypair()
+
+const publicKey = new PublicKey(wallet._keypair.publicKey)
+const secretKey = wallet._keypair.secretKey
+
+const getWalletBalance = async() => {
+    try {
+        const connection = new Connection(clusterApiUrl('devnet'), 'confirmed')
+        const walletBalance = await connection.getBalance(publicKey)
+        console.log(`Wallet balance is ${walletBalance}`)
+    } catch(err) {
+        console.error(err)
+    }
+}
+const airDropSol = async () => {
+    try {
+      const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
+      const airdropSignature = await connection.requestAirdrop(
+        wallet.publicKey,
+        2 * LAMPORTS_PER_SOL
+      );
+
+      const latestBlockHash = await connection.getLatestBlockhash();
+
+      await connection.confirmTransaction({
+        blockhash: latestBlockHash.blockhash,
+        lastValidBlockHeight: latestBlockHash.lastValidBlockHeight,
+        signature: airdropSignature,
+      });
+    } catch (error) {
+      console.error(error);
+    }
+};
+
+const main = async() => {
+    await getWalletBalance()
+    await airDropSol()
+    await getWalletBalance()
+}
+main()
+
+
+// console.log(publicKey)
+// console.log(secretKey)
